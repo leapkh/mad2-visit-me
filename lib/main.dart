bdd4b0a3177928e19a5175396e4c14104e788beb
Bunleap <leap.b@pipay.com>
2022-11-18 18:22:02 +0700

    UI Design

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 
 Widget app() {
   return MaterialApp(
-    home: placeDetailScreen(),
+    home: searchScreen(),
   );
 }
 
@@ -229,7 +229,7 @@ Widget placeItemWidget(BuildContext context, int index) {
         Expanded(
             child: Image.asset(
           'lib/image/${place.imageName}',
-              fit: BoxFit.cover,
+          fit: BoxFit.cover,
         )),
         Text(place.name)
       ],
@@ -280,5 +280,86 @@ Widget iconAndInfoWidget(IconData iconData, String info) {
       ),
       Expanded(child: Text(info))
     ],
+  );
+}
+
+Widget searchScreen() {
+  final body = SingleChildScrollView(
+    child: Column(
+      children: [
+        toolbarWidget(),
+        sectionTitleWidget('Recent Searches', Icons.recent_actors),
+        recentSearchItemsWidget(),
+        sectionTitleWidget('Advanced Searches', Icons.search),
+      ],
+    ),
+  );
+
+  return Scaffold(
+    body: body,
+  );
+}
+
+List<String> keywords = [
+  'royal palace',
+  'national museum',
+  'river side',
+  'royal palace',
+  'national museum',
+  'river side',
+  'Angkor Wat'
+];
+
+Widget recentSearchItemsWidget() {
+  /*final items = keywords.map((keyword) {
+    return recentSearchItemWidget(keyword);
+  }).toList();
+
+  return Wrap(
+    children: items,
+  );*/
+
+  final favoriteKeywords = keywords.where((keyword) {
+    return keyword.contains('s');
+  }).toList();
+
+  return Wrap(
+    children: favoriteKeywords
+        .map((keyword) => recentSearchItemWidget(keyword))
+        .toList(),
+  );
+}
+
+Widget recentSearchItemWidget(String keyword) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 4),
+    child: Chip(
+        label: Text(
+      keyword,
+    )),
+  );
+}
+
+Widget toolbarWidget() {
+  return Container(
+    padding: EdgeInsets.all(8),
+    height: 54,
+    color: Colors.green,
+    child: TextField(),
+  );
+}
+
+Widget sectionTitleWidget(String title, IconData iconData) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 16),
+    child: Row(
+      children: [
+        Text(
+          title,
+          style: TextStyle(fontSize: 18),
+        ),
+        Icon(iconData)
+      ],
+    ),
   );
 }
